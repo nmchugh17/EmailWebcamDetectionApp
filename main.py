9e1c3121a49d7d8825f5bf16a7ef7d2ea45aceb5
nmchugh17 <nmchu17@gmail.com>
2024-07-13 23:43:15 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+import cv2
+import time
+import glob
+import os
+from emailing import send_email
+
+
+
+# Parameter = 0 - uses main camera, Parameter = 1 - uses secondary camera attached
+video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+first_frame = None
+time.sleep(1)
+
+# Initialize the background subtractor
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+# first_frame = None
+status_list = [0]
+count = 1
+
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
+
+while True:
+    status = 0
+    check, frame = video.read()
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    # Apply the background subtractor to get the foreground mask
+    fgmask = fgbg.apply(gray_frame_gau)
+    thresh_frame = cv2.threshold(fgmask, 45, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    cv2.imshow("My video", dil_frame)
+
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email(image_with_object)
+        clean_folder()
+
+    cv2.imshow("Video", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+video.release()
